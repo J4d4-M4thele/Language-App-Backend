05e8d49a99932a8c00b4cbc2efdd5c82fc392627
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-15 10:26:03 +0200

    first commit

---
+++ src/main/java/com/languagews/languagesystem/exception/LanguageNotFoundException.java
@@ -0,0 +1,7 @@
+package com.languagews.languagesystem.exception;
+
+public class LanguageNotFoundException extends RuntimeException{
+    public LanguageNotFoundException(String msg) {
+        super(msg);
+    }
+}
